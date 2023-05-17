15af74f4a183ac9385476dfd216436704f9eb898
ArzuGojayeva <125992371+ArzuGojayeva@users.noreply.github.com>
2023-05-17 11:11:15 +0400

    NEST

--- NEST/NEST/Areas/NestAdmin/Controllers/TagsController.cs
+++ NEST/NEST/Areas/NestAdmin/Controllers/TagsController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NEST.DAL;
+using NEST.Models;
 
 namespace NEST.Areas.NestAdmin.Controllers
 {
+    [Area("NestAdmin")]
     public class TagsController : Controller
     {
-        public IActionResult Index()
+        
+        private readonly AppDbContext _context;
+        public TagController(AppDbContext context)
+        {
+            _context = context;
+            
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tags.ToListAsync());
+        }
+        public IActionResult Create()
         {
             return View();
         }
+        public async Task<IActionResult> Create(Tag tag)
+        {
+            if (!ModelState.IsValid) return View();
+            await _context.Tags.AddAsync(tag);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
+
